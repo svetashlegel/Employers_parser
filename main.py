6c6a814bc1f5404464ab6fb3314beaf06459e2f8
shlegel <sveta.shlegel@mail.ru>
2023-07-09 20:44:56 +0300

    Added main code (working with DB)

--- main.py
+++ main.py
@@ -9,14 +9,12 @@ db_manager = DBManager(database)
 db_manager.truncate_tables()
 for company in companies:
     comp_data = get_company_by_name(company)
-    url = f"https://hh.ru/employer/{comp_data['id']}"
-    db_manager.save_employer(comp_data['id'], comp_data['name'], url)
+    db_manager.save_employer(comp_data['id'], comp_data['name'], comp_data['alternate_url'])
 
 emp_data = db_manager.get_employers()
 for emp in emp_data:
     vacancies = get_vacancies(emp[0])
     for vac in vacancies['items']:
-        url = f"https://hh.ru/vacancy/{vac['id']}"
         if not vac['salary']:
             salary = None
         else:
@@ -26,4 +24,14 @@ for emp in emp_data:
                 salary = vac['salary']['to']
         db_manager.save_vacancy(vac['id'], vac['name'], emp[0], salary, vac['alternate_url'])
 
+vacancies_per_company = db_manager.get_companies_and_vacancies_count()
+print(vacancies_per_company)
+all_vacancies = db_manager.get_all_vacancies()
+print(all_vacancies)
+avg_salary = db_manager.get_avg_salary()
+print(avg_salary)
+vacancies_with_higher_salary = db_manager.get_vacancies_with_higher_salary()
+print(vacancies_with_higher_salary)
+vacancies_with_keyword = db_manager.get_vacancies_with_keyword('разработчик')
+print(vacancies_with_keyword)
 db_manager.close_connection()
