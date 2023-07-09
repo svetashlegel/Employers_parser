31bb390c179405c3f0fd7300a0b235e97e50c666
shlegel <sveta.shlegel@mail.ru>
2023-07-09 11:44:43 +0300

    Defined structure

---
+++ src/api_func.py
@@ -0,0 +1,8 @@
+def get_company_by_name(company_name):
+    """Возвращает id компании по ее названию"""
+    pass
+
+
+def get_vacancies(employer_id):
+    """Получает список вакансий компании"""
+    pass
