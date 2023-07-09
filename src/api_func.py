6053c4bab2a791a49b592cdd13615b784ec3b89d
shlegel <sveta.shlegel@mail.ru>
2023-07-09 12:10:11 +0300

    Added fuctions to work with API

--- src/api_func.py
+++ src/api_func.py
@@ -1,8 +1,28 @@
+import requests
+import json
+
+
 def get_company_by_name(company_name):
     """Возвращает id компании по ее названию"""
-    pass
+    params = {
+        'text': company_name,
+        'area': 1
+    }
+    req = requests.get('https://api.hh.ru/employers', params)
+    data = req.content.decode()
+    req.close()
+    dataj = json.loads(data)
+    return dataj['items'][0]
 
 
 def get_vacancies(employer_id):
     """Получает список вакансий компании"""
-    pass
+    params = {
+        'employer_id': employer_id,
+        'area': 1
+    }
+    req = requests.get('https://api.hh.ru/vacancies', params)
+    data = req.content.decode()
+    req.close()
+    dataj = json.loads(data)
+    return dataj
